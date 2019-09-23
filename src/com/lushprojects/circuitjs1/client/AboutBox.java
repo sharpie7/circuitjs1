df95cbe34fdb8aafdae87074c991f6e306f65982
pf <paul@falstad.com>
2019-09-23 11:20:39 -0700

    add thanks to Lucio Sciamanna for Italian translation

--- src/com/lushprojects/circuitjs1/client/AboutBox.java
+++ src/com/lushprojects/circuitjs1/client/AboutBox.java
@@ -45,6 +45,7 @@ public class AboutBox extends PopupPanel {
 		"Thomas Reitinger for the German translation; " +
 		"Krystian Sławiński for the Polish translation; " +
 		"Usevalad Khatkevich for the Russian translation; " +
+		"Lucio Sciamanna for the Italian translation; " +
 		"Andre Adrian for improved emitter coupled oscillator; Felthry for many examples; " +
 		"Colin Howell for code improvements.</p>"+
 		"<p style=\"font-size:9px\">This program is free software: you can redistribute it and/or modify it "+
