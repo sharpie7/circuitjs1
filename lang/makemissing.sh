165543317e2b6b106c57e84d5a903cba8632c314
pf <paul@falstad.com>
2021-05-24 16:36:46 -0700

    update missing strings add Taiwan Chinese

--- lang/makemissing.sh
+++ lang/makemissing.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in da de es fr it nb pl pt ru zh
+for i in da de es fr it nb pl pt ru zh zh-tw
 do
   python findmissing.py ../src/com/lushprojects/circuitjs1/public/locale_$i.txt > missing_$i.txt
 done
