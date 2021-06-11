d2256ed7c19303f5059272958106c7feaf0e210a
dogsong <55410457+nulldg@users.noreply.github.com>
2021-06-11 20:15:42 +0100

    Cleanup SeqGenElm.java
    "One shot" is now stored as a flag, but the boolean still exists for backcomp. This also adds a reset pin.

--- src/com/lushprojects/circuitjs1/client/SeqGenElm.java
+++ src/com/lushprojects/circuitjs1/client/SeqGenElm.java
@@ -21,196 +21,133 @@ package com.lushprojects.circuitjs1.client;
 
 // contributed by Edward Calver
 
-    class SeqGenElm extends ChipElm {
-	boolean hasReset() {return false;}
-	public SeqGenElm(int xx, int yy) { super(xx, yy); }
-	public SeqGenElm(int xa, int ya, int xb, int yb, int f,
-			    StringTokenizer st) {
-	    super(xa, ya, xb, yb, f, st);
-	data = (short)(new Integer(st.nextToken()).intValue());
-	if(st.hasMoreTokens()){oneshot=new Boolean(st.nextToken()).booleanValue();position=8;}
+class SeqGenElm extends ChipElm {
+	final int FLAG_HAS_RESET = 2; //Flag for backwards compatibility
+	final int FLAG_ONE_SHOT = 4;
+	
+	public SeqGenElm(int xx, int yy) {
+		super(xx, yy);
+		flags |= FLAG_HAS_RESET;
+		setupPins();
+		allocNodes();
 	}
-	short data=0;
-byte position=0;
-boolean oneshot=false;
-double lastchangetime=0;
-	boolean clockstate=false;
-	String getChipName() { return "Sequence generator"; }
+	public SeqGenElm(int xa, int ya, int xb, int yb, int f, StringTokenizer st) {
+		super(xa, ya, xb, yb, f, st);
+		data = (short)(new Integer(st.nextToken()).intValue());
+		if (st.hasMoreTokens()) {
+			if (new Boolean(st.nextToken()).booleanValue()) { //Backwards compatibility
+				flags |= FLAG_ONE_SHOT;
+				setupPins();
+				allocNodes();
+			}
+			position = 8;
+		}
+	}
+	
+	short data = 0;
+	byte position = 0;
+	double lastchangetime = 0;
+	boolean clockstate = false;
+	String getChipName() { return "sequence generator"; }
 	void setupPins() {
-	    sizeX = 2;
-	    sizeY = 2;
-	    pins = new Pin[getPostCount()];
-
-	    pins[0] = new Pin(0, SIDE_W, "");
-	    pins[0].clock=true;
-	    pins[1] = new Pin(1, SIDE_E, "Q");
-	    pins[1].output=true;
+		sizeX = 2;
+		sizeY = 2;
+		pins = new Pin[getPostCount()];
+		
+		pins[0] = new Pin(0, SIDE_W, "");
+		pins[0].clock = true;
+		pins[1] = new Pin(1, SIDE_E, "Q");
+		pins[1].output = true;
+		if (hasReset())
+			pins[2] = new Pin(1, SIDE_W, "R");
 	}
-	int getPostCount() {
-	    return 2;
+	int getPostCount() { return hasReset() ? 3 : 2; }
+	int getVoltageSourceCount() { return 1; }
+	boolean hasOneShot() { return (flags & FLAG_ONE_SHOT) != 0; }
+	boolean hasReset() { return (flags & FLAG_HAS_RESET) != 0; }
+	
+	void getNextBit() {
+		pins[1].value = ((data >>> position) & 1) != 0;
+		position++;
 	}
-	int getVoltageSourceCount() {return 1;}
-
-void GetNextBit()
-{
-if(((data>>>position)&1)!=0)pins[1].value=true;
-else pins[1].value=false;
-position++;
-}
-
+	
 	void execute() {
-		if(oneshot)
-		{
-			if(sim.t-lastchangetime>0.005)
-			{
-				if(position<=8)GetNextBit();
-				lastchangetime=sim.t;
+		if (hasOneShot()) {
+			if (sim.t - lastchangetime > 0.005) {
+				if (position <= 8)
+					getNextBit();
+				lastchangetime = sim.t;
 			}
 		}
-		if(pins[0].value&&!clockstate)
-		{
-			clockstate=true;
-			if(oneshot)
-			{
-				position=0;
-			}
-			else
-			{
-				GetNextBit();
-				if(position>=8)position=0;
+		if (hasReset() && pins[2].value) {
+			// Suspended state (RESET raised)
+			position = (byte) (hasOneShot() ? 8 : 0);
+			pins[1].value = false;
+			clockstate = pins[0].value;
+		} else {
+			// Normal operation
+			if (pins[0].value != clockstate) {
+				// Edge transition
+				clockstate = pins[0].value;
+				if (clockstate) {
+					// Rising-edge event
+					clockstate = true;
+					if (hasOneShot()) {
+						position = 0;
+					} else {
+						getNextBit();
+						if(position >= 8)
+							position = 0;
+					}
+				}
 			}
 		}
-		if(!pins[0].value)clockstate=false;
-			
 	}
 	int getDumpType() { return 188; }
-
+	
 	String dump(){
-	    return super.dump()+" "+data+" "+oneshot;
+		return super.dump() + " " + data;
 	}
 	public EditInfo getEditInfo(int n) {
-	//My code
-	    if (n == 0) {
-		EditInfo ei = new EditInfo("", 0, -1, -1);
-		ei.checkbox = new Checkbox("Bit 0 set", (data&1) != 0);
-		return ei;
-	    }
-
-	    if (n == 1) {
-		EditInfo ei = new EditInfo("", 0, -1, -1);
-		ei.checkbox = new Checkbox("Bit 1 set", (data&2) != 0);
-		return ei;
-	    }
-	    if (n == 2) {
-		EditInfo ei = new EditInfo("", 0, -1, -1);
-		ei.checkbox = new Checkbox("Bit 2 set", (data&4) != 0);
-		return ei;
-	    }
-	    if (n == 3) {
-		EditInfo ei = new EditInfo("", 0, -1, -1);
-		ei.checkbox = new Checkbox("Bit 3 set", (data&8) != 0);
-		return ei;
-	    }
-
-	    if (n == 4) {
-		EditInfo ei = new EditInfo("", 0, -1, -1);
-		ei.checkbox = new Checkbox("Bit 4 set", (data&16) != 0);
-		return ei;
-	    }
-	    if (n == 5) {
-		EditInfo ei = new EditInfo("", 0, -1, -1);
-		ei.checkbox = new Checkbox("Bit 5 set", (data&32) != 0);
-		return ei;
-	    }
-
-	    if (n == 6) {
-		EditInfo ei = new EditInfo("", 0, -1, -1);
-		ei.checkbox = new Checkbox("Bit 6 set", (data&64) != 0);
-		return ei;
-	    }
-
-	    if (n == 7) {
-		EditInfo ei = new EditInfo("", 0, -1, -1);
-		ei.checkbox = new Checkbox("Bit 7 set", (data&128) != 0);
-		return ei;
-	    }
-	    if (n == 8) {
-		EditInfo ei = new EditInfo("", 0, -1, -1);
-		ei.checkbox = new Checkbox("One shot", oneshot);
-		return ei;
+		if (n < 2)
+			return super.getEditInfo(n);
+		if (n < 10) {
+			int bitIndex = n - 2;
+			EditInfo ei = new EditInfo("", 0, -1, -1);
+			ei.checkbox = new Checkbox("Bit "+(bitIndex+1)+" set", (data & (1<<bitIndex)) != 0);
+			return ei;
 		}
-	    return null;
+		if (n == 10) {
+			EditInfo ei = new EditInfo("", 0, -1, -1);
+			ei.checkbox = new Checkbox("One shot", hasOneShot());
+			return ei;
+		}
+		return null;
 	}
 	public void setEditValue(int n, EditInfo ei) {
-	    if (n == 0) {
-		if (ei.checkbox.getState())
-		    data|=1;
-		else
-		    data&= ~1;
-		setPoints();
-	    }
-	    if (n == 1) {
-		if (ei.checkbox.getState())
-		    data|=2;
-		else
-		    data&= ~2;
-		setPoints();
-	    }
-	    if (n == 2) {
-		if (ei.checkbox.getState())
-		    data|=4;
-		else
-		    data&= ~4;
-		setPoints();
-	    }
-	    if (n == 3) {
-		if (ei.checkbox.getState())
-		    data|=8;
-		else
-		    data&= ~8;
-		setPoints();
-	    }
-	    if (n == 4) {
-		if (ei.checkbox.getState())
-		    data|=16;
-		else
-		    data&= ~16;
-		setPoints();
-	    }
-	    if (n == 5) {
-		if (ei.checkbox.getState())
-		    data|=32;
-		else
-		    data&= ~32;
-		setPoints();
-	    }
-	    if (n == 6) {
-		if (ei.checkbox.getState())
-		    data|=64;
-		else
-		    data&= ~64;
-		setPoints();
-	    }
-	    if (n == 7) {
-		if (ei.checkbox.getState())
-		    data|=128;
-		else
-		    data&= ~128;
-		setPoints();
-	    }
-	    if (n == 8) {
-		if (ei.checkbox.getState())
-		{
-		oneshot=true;
-		position=8;
+		if (n < 2) {
+			super.setEditValue(n, ei);
+			return;
 		}
-		else
-		{
-		    position=0;
-		    oneshot=false;
+		if (n < 10) {
+			int bitIndex = n - 2;
+			int bit = 1 << bitIndex;
+			if (ei.checkbox.getState())
+				data |= bit;
+			else
+				data &= ~bit;
+			setPoints();
+			return;
 		}
-	    }
-
+		if (n == 10) {
+			if (ei.checkbox.getState()) {
+				flags |= FLAG_ONE_SHOT;
+				position = 8;
+			} else {
+				flags &= ~FLAG_ONE_SHOT;
+				position = 0;
+			}
+			return;
+		}
 	}
-
-    }
+}
