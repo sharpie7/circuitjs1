f00966be30414eef781973fcd185a1aa8bddbe23
pf <paul@falstad.com>
2021-07-29 12:00:58 -0700

    fix units

--- src/com/lushprojects/circuitjs1/client/DelayBufferElm.java
+++ src/com/lushprojects/circuitjs1/client/DelayBufferElm.java
@@ -111,7 +111,7 @@ package com.lushprojects.circuitjs1.client;
 	    if (n == 0)
 		return new EditInfo("Delay (s)", delay, 0, 0);
 	    if (n == 1)
-		return new EditInfo("Threshold (s)", threshold, 0, 0);
+		return new EditInfo("Threshold (V)", threshold, 0, 0);
 	    if (n == 2)
 		return new EditInfo("High Voltage", highVoltage, 0, 0);
 	    return null;
