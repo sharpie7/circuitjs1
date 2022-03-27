9d44b35379c2b27977e27af8e4a3d0969093656e
Paul Falstad <paul@falstad.com>
2022-03-27 10:32:37 -0700

    2.6.8

--- src/com/lushprojects/circuitjs1/client/circuitjs1.java
+++ src/com/lushprojects/circuitjs1/client/circuitjs1.java
@@ -73,7 +73,7 @@ import com.google.gwt.user.client.Window.ClosingEvent;
 
 public class circuitjs1 implements EntryPoint {
 
-	public static final String versionString="2.6.7js";
+	public static final String versionString="2.6.8js";
 	
 	// Set to true if the server runs the shortrelay.php file in the same directory as the circuit simulator
 	public static final boolean shortRelaySupported = true;
