sharpie7/circuitjs1

commit 3616815ef3078cc7cb35f2e4c1d3096ea96b6ec3

improve handling of fast current display (#680)

src/com/lushprojects/circuitjs1/client/CircuitElm.java
@@ -22,9 +22,11 @@ package com.lushprojects.circuitjs1.client;
 import java.util.Vector;
 
 import com.google.gwt.canvas.dom.client.CanvasGradient;
+import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.canvas.dom.client.Context2d.LineCap;
 import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.storage.client.Storage;
+import com.google.gwt.user.client.Random;
 
 // circuit element class
 public abstract class CircuitElm implements Editable {
@@ -360,6 +362,8 @@ public abstract class CircuitElm implements Editable {
 	return a;
     }
 
+    final int CURRENT_TOO_FAST = 100;
+
     // draw current dots from point a to b
     void drawDots(Graphics g, Point pa, Point pb, double pos) {
 	 if ((!sim.simIsRunning()) || pos == 0 || !sim.dotsCheckItem.getState())
@@ -369,6 +373,20 @@ public abstract class CircuitElm implements Editable {
 	double dn = Math.sqrt(dx*dx+dy*dy);
 	g.setColor(currentColor);
 	int ds = 16;
+	if (pos == CURRENT_TOO_FAST || pos == -CURRENT_TOO_FAST) {
+	    // current is moving too fast, avoid aliasing by drawing dots at
+	    // random position with transparent yellow line underneath
+	    g.save();
+	    Context2d ctx = g.context;
+	    ctx.setLineWidth(4);
+	    ctx.setGlobalAlpha(.5);
+	    ctx.beginPath();
+	    ctx.moveTo(pa.x, pa.y);
+	    ctx.lineTo(pb.x, pb.y);
+	    ctx.stroke();
+	    g.restore();
+	    pos = Random.nextDouble()*ds;
+	}
 	pos %= ds;
 	if (pos < 0)
 	    pos += ds;
@@ -897,13 +915,9 @@ public abstract class CircuitElm implements Editable {
 	 if (!sim.simIsRunning())
 	    return cc;
 	double cadd = cur*currentMult;
-	/*if (cur != 0 && cadd <= .05 && cadd >= -.05)
-	  cadd = (cadd < 0) ? -.05 : .05;*/
+	if (cadd > 6 || cadd < -6)
+	    return CURRENT_TOO_FAST;
 	cadd %= 8;
-	/*if (cadd > 8)
-	  cadd = 8;
-	  if (cadd < -8)
-	  cadd = -8;*/
 	return cc + cadd;
     }
     

commit d44d6823605906dbec6dcd009f68a552fe4640de

fix current animation for lamps broken by #680

src/com/lushprojects/circuitjs1/client/CircuitElm.java
@@ -400,6 +400,12 @@ public abstract class CircuitElm implements Editable {
 	}
     }
 
+    double addCurCount(double c, double a) {
+	if (c == CURRENT_TOO_FAST || c == -CURRENT_TOO_FAST)
+	    return c;
+	return c+a;
+    }
+    
     Polygon calcArrow(Point a, Point b, double al, double aw) {
 	Polygon poly = new Polygon();
 	Point p1 = new Point();

src/com/lushprojects/circuitjs1/client/LampElm.java
@@ -119,13 +119,13 @@ package com.lushprojects.circuitjs1.client;
 	    updateDotCount();
 	    if (sim.dragElm != this) {
 		drawDots(g, point1, lead1, curcount);
-		double cc = curcount+(dn-16)/2;
+		double cc = addCurCount(curcount, (dn-16)/2);
 		drawDots(g, lead1,  filament[0], cc);
-		cc += filament_len;
+		cc = addCurCount(cc, filament_len);
 		drawDots(g, filament[0], filament[1], cc);
-		cc += 16;
+		cc = addCurCount(cc, 16);
 		drawDots(g, filament[1], lead2, cc);
-		cc += filament_len;
+		cc = addCurCount(cc, filament_len);
 		drawDots(g, lead2, point2, curcount);
 	    }
 	    drawPosts(g);