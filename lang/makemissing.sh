833d7ac7c7213e9bd23650054a44e18e01dd8c15
Paul Falstad <paul@falstad.com>
2022-04-23 13:55:37 -0700

    add reminder

--- lang/makemissing.sh
+++ lang/makemissing.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo run getstrings first
 for i in da de es fr it nb pl pt ru zh zh-tw
 do
   python findmissing.py ../src/com/lushprojects/circuitjs1/public/locale_$i.txt > missing_$i.txt
