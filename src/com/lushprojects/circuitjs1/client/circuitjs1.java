6212a82544cc7c0e225d52c40c36da9f1230154e
Mervill <mervills.email@gmail.com>
2022-08-19 13:52:48 -0700

    stale comments

--- src/com/lushprojects/circuitjs1/client/circuitjs1.java
+++ src/com/lushprojects/circuitjs1/client/circuitjs1.java
@@ -21,42 +21,6 @@ package com.lushprojects.circuitjs1.client;
 
 import java.util.HashMap;
 
-//CirSim.java (c) 2010 - 2017 by Paul Falstad
-//GWT conversion (c) 2015 - 2017 by Iain Sharp
-
-//Version History
-
-//v1.9.1js 16-11-06 Iain Sharp
-//Add import of file from CORS compatible link
-//v1.9.0js 16-11-06 Iain Sharp
-// Add URL-shortener and Dropbox integration
-//v1.8.0js 16-10-30 Iain Sharp
-// Incorporate latest Falstad updates. Improvements to UI and bug fixes
-//v1.0.1 15-06-15
-//Convert source code to GPLv2
-//Incorporate example files in to project
-//v1.0.0 15-06-05
-//Import/export to/from text now fixed
-//v0.1.3 15-06-03
-//Handles appear on components when dragged
-//Improved integration of potentiometers and VarRails with sliders - colour changes and support
-//for scroll wheel.
-//v0.1.2 15-06-01
-//Automatic selection of post drag mode when user is near a handle in select mode
-//Visual appearance of handles changed
-//Accepts "2k2" style engineers short-hand for component values
-//Menus prettified
-//v0.1.1 
-//Bug fix for PNP transistors and past
-//v0.1.0 - 
-//Initial test release on web
-
-//ToDos
-// Scope improvements
-//UI improvements
-//Potentiometer - improve drawing code
-//Coil drawing - find out why my alternative code doesn't work
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.logical.shared.ResizeEvent;
@@ -178,23 +142,13 @@ public class circuitjs1 implements EntryPoint {
         mysim.init();
 
         Window.addResizeHandler(new ResizeHandler() {
-
             public void onResize(ResizeEvent event) {
                 mysim.setCanvasSize();
                 mysim.setiFrameHeight();
-
             }
         });
 
-        /*
-         * Window.addWindowClosingHandler(new Window.ClosingHandler() {
-         * 
-         * public void onWindowClosing(ClosingEvent event) {
-         * event.setMessage("Are you sure?"); } });
-         */
-
         mysim.updateCircuit();
-
     }
 
 }
