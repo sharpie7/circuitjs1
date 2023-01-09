d65a681fe68741fd3cc48fe5a879cfdc211f3298
Paul Falstad <paul@falstad.com>
2023-01-08 22:04:53 -0800

    update missing strings

--- lang/makemissing.sh
+++ lang/makemissing.sh
@@ -2,5 +2,5 @@
 echo run getstrings first
 for i in da de es fr it nb pl pt ru zh zh-tw
 do
-  python findmissing.py ../src/com/lushprojects/circuitjs1/public/locale_$i.txt > missing_$i.txt
+  python3 findmissing.py ../src/com/lushprojects/circuitjs1/public/locale_$i.txt > missing_$i.txt
 done
