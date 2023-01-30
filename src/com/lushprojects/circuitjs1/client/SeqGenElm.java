216154b9e8f68ced07524cd3020ea13f85f2faf6
Paul Falstad <paul@falstad.com>
2023-01-29 21:31:05 -0800

    add reset input for sequence generator (#786)

--- src/com/lushprojects/circuitjs1/client/SeqGenElm.java
+++ src/com/lushprojects/circuitjs1/client/SeqGenElm.java
@@ -39,7 +39,7 @@ class SeqGenElm extends ChipElm {
 		bitCount = 8;
 		data = new int[] { 0 };
 		flags |= FLAG_NEW_VERSION;
-		//flags |= FLAG_HAS_RESET; // Uncomment this if somebody asks for a RESET pin on the SeqGen
+		flags |= FLAG_HAS_RESET;
 		setupPins();
 		allocNodes();
 	}
@@ -85,7 +85,7 @@ class SeqGenElm extends ChipElm {
 		pins[1] = new Pin(1, SIDE_E, "Q");
 		pins[1].output = true;
 		if (hasReset())
-			pins[2] = new Pin(1, SIDE_W, "R");
+		    pins[2] = new Pin(1, SIDE_W, "R");
 	}
 	double getVoltageDiff() {
 		return volts[1];
@@ -120,8 +120,8 @@ class SeqGenElm extends ChipElm {
 		if (hasReset() && pins[2].value) {
 			// Suspended state (RESET raised)
 			bitPosition = 0;
-			pins[1].value = data.length > 0 && (data[0] & 1) != 0;
 			clockstate = pins[0].value;
+			nextBit();
 		} else {
 			// Normal operation
 			if (pins[0].value != clockstate) {
