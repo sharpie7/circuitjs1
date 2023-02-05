392fa35150af76b1ea8b3aafdc5e2e9f60b7b6b6
Paul Falstad <paul@falstad.com>
2023-02-04 19:01:41 -0800

    2.7.6

--- src/com/lushprojects/circuitjs1/client/circuitjs1.java
+++ src/com/lushprojects/circuitjs1/client/circuitjs1.java
@@ -36,7 +36,7 @@ import com.lushprojects.circuitjs1.client.util.Locale;
 
 public class circuitjs1 implements EntryPoint {
 
-    public static final String versionString = "2.7.5js";
+    public static final String versionString = "2.7.6js";
 
     // Set to true if the server runs the shortrelay.php file in the same directory
     // as the circuit simulator
